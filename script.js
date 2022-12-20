ac10c62890fb20af068ff2fb126600e28cb2319b
Effiong Prince <95102901+Effiongprince19@users.noreply.github.com>
2022-12-20 15:39:27 +0100

    Added mode switcher functionality

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const mode = document.querySelector('.mode-toggler');
+const container = document.querySelector('.container');
+const boxes = document.querySelectorAll('.box');
 
-
-// mode.addEventListener('click', modeToggle)
-
-// function modeToggle () {
-//  mode.classList.toggle('light-mode');
-//  console.log('clicked')
-// }
 
 mode.addEventListener('click', () => {
     console.log('clicked')
-    mode.classList.toggle('light-mode')
+    mode.classList.toggle('light-mode');
+    container.classList.toggle('light-mode');
+    boxes.classList.toggle('light-mode');
 })
