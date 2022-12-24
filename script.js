604c7785d9203cb808dc82738d54415434ab217a
Effiong Prince <95102901+Effiongprince19@users.noreply.github.com>
2022-12-24 20:33:36 +0100

    Updated my fonts

--- script.js
+++ script.js
@@ -7,5 +7,17 @@ mode.addEventListener('click', () => {
     console.log('clicked')
     mode.classList.toggle('light-mode');
     container.classList.toggle('light-mode');
-    boxes.classList.toggle('light-mode');
+    boxes.style.transform = 'scale(0.9)';
 })
+
+// boxes.addEventListener('mouseover', () => {
+//     boxes.style.transform= 'scale(0.1)'
+// })
+
+// function bigImg(x) {
+//   x.style.transform = "scale(0.9)";
+// }
+
+// function normalImg(x) {
+//   x.style.height = "scale(1)";
+// }
